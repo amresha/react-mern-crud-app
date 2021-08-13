6233901f607f2ef60831b208394aea0a98a07752
emmadigital <emma.digital.official@gmail.com>
2021-08-13 14:25:00 +0300

    react mern stack crud app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import "bootstrap/dist/css/bootstrap.css";
+import "./App.css";
+
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import CreateEmployee from "./components/create-employee.component";
+import EditEmployee from "./components/edit-employee.component";
+import EmployeeList from "./components/employee-list.component";
 
 function App() {
-  return (
+  const createError = require('http-errors');
+
+  return (<Router>
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Navbar bg="dark" variant="dark">
+          <Container>
+
+            <Navbar.Brand>
+              <Link to={"/create-employee"} className="nav-link">
+                React MERN Stack App
+              </Link>
+            </Navbar.Brand>
+
+            <Nav className="justify-content-end">
+              <Nav>
+                <Link to={"/create-employee"} className="nav-link">
+                  Create Employee
+                </Link>
+              </Nav>
+
+              {/* <Nav>
+                <Link to={"/edit-employee/:id"} className="nav-link">
+                  Edit Employee
+                </Link>
+              </Nav> */}
+
+              <Nav>
+                <Link to={"/employee-list"} className="nav-link">
+                Employee List
+                </Link>
+              </Nav>
+            </Nav>
+
+          </Container>
+        </Navbar>
       </header>
+
+      <Container>
+        <Row>
+          <Col md={12}>
+            <div className="wrapper">
+              <Switch>
+                <Route exact path='/' component={CreateEmployee} />
+                <Route path="/create-employee" component={CreateEmployee} />
+                <Route path="/edit-employee/:id" component={EditEmployee} />
+                <Route path="/employee-list" component={EmployeeList} />
+              </Switch>
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </div>
-  );
+  </Router>);
 }
 
 export default App;
